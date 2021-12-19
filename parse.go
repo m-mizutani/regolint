6472688934e28ea2bf1f444bd2eee3141f49d270
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2021-12-19 17:45:44 +0900

    Change directory normalization

--- parse.go
+++ parse.go
@@ -14,7 +14,7 @@ type RegoFile struct {
 	Path []string    `json:"path"`
 	Rego *ast.Module `json:"rego"`
 
-	filePath string
+	filePath string // original given file path
 }
 
 func loadDirs(dirs ...string) ([]*RegoFile, error) {
@@ -32,6 +32,11 @@ func loadDirs(dirs ...string) ([]*RegoFile, error) {
 }
 
 func loadFiles(dir string) ([]*RegoFile, error) {
+	basePath, err := filepath.Abs(dir)
+	if err != nil {
+		return nil, goerr.Wrap(err)
+	}
+
 	var regoFiles []*RegoFile
 	handler := func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
@@ -39,27 +44,31 @@ func loadFiles(dir string) ([]*RegoFile, error) {
 		}
 
 		if strings.HasSuffix(path, ".rego") {
-			path := loader.CleanPath(path)
-			logger.With("path", path).Debug("load .rego")
+			relPath, err := filepath.Rel(basePath, path)
+			if err != nil {
+				return goerr.Wrap(err)
+			}
+
+			logger.With("path", path).With("rel", relPath).Debug("load .rego")
 			regoFile, err := loader.RegoWithOpts(path, ast.ParserOptions{
 				ProcessAnnotation: true,
 			})
 			if err != nil {
-				return goerr.Wrap(err).With("path", path)
+				return goerr.Wrap(err).With("path", relPath)
 			}
-			logger.With("path", path).With("rego", regoFile).Trace("loaded")
+			logger.With("path", relPath).With("rego", regoFile).Trace("loaded")
 
 			regoFiles = append(regoFiles, &RegoFile{
 				filePath: path,
 				Rego:     regoFile.Parsed,
-				Path:     strings.Split(path, string(filepath.Separator)),
+				Path:     strings.Split(relPath, string(filepath.Separator)),
 			})
 		}
 
 		return nil
 	}
 
-	if err := filepath.WalkDir(dir, handler); err != nil {
+	if err := filepath.WalkDir(basePath, handler); err != nil {
 		return nil, goerr.Wrap(err)
 	}
 	return regoFiles, nil
